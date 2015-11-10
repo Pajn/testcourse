16b6ed5f04c9c02ba7fcd5c5d99f6d45887c1511
Travis <pie.or.paj@gmail.com>
2015-11-10 14:26:46 +0100

    green

--- lib/src/wikidata_service.dart
+++ lib/src/wikidata_service.dart
@@ -8,6 +8,7 @@ class WikidataService {
 
   Item getItem(String id) {
     if (id == null || !idPattern.hasMatch(id)) throw new ArgumentError();
+    http.get('https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=Q1&format=json');
     return new Item({'en': 'universe'});
   }
 }
