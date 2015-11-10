051bfa7896ce8ec77d98b6c8fa2e32e3a7c67a78
Travis <pie.or.paj@gmail.com>
2015-11-10 13:37:53 +0100

    green

--- lib/src/wikidata_service.dart
+++ lib/src/wikidata_service.dart
@@ -1,6 +1,8 @@
 class WikidataService {
+  final RegExp idPattern = new RegExp(r'Q\d+');
+
   Item getItem(String id) {
-    if (id == null) throw new ArgumentError();
+    if (id == null || !idPattern.hasMatch(id)) throw new ArgumentError();
     return new Item();
   }
 }
