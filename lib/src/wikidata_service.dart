02204e97fa8fbe30cc40109789fc639df9373e8e
Travis <pie.or.paj@gmail.com>
2015-11-10 13:53:05 +0100

    refactor

--- lib/src/wikidata_service.dart
+++ lib/src/wikidata_service.dart
@@ -3,12 +3,12 @@ class WikidataService {
 
   Item getItem(String id) {
     if (id == null || !idPattern.hasMatch(id)) throw new ArgumentError();
-    return new Item();
+    return new Item({'en': 'universe'});
   }
 }
 
 class Item {
-  final Map<String, String> label = {
-    'en': 'universe',
-  };
+  final Map<String, String> label;
+
+  Item(this.label);
 }
